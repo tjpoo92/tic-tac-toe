29415da7cacf8d59f3e501cc5d6fcc00083a1284
Tyler Pooschke <tjpoo92@gmail.com>
2021-03-05 13:52:38 -0600

    added players and player logic

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ let switcher = (function () {
 })();
 
 let gameBoard = (function () {
-  let i = 0;
+  let counter = 0;
   let xsquares = [];
   let osquares = [];
   const winningCombination = [
@@ -48,25 +48,59 @@ let gameBoard = (function () {
   ];
 
   //DOM Cache
-  const gameBoard = document.querySelectorAll("button");
+  const gameBoard = document.querySelectorAll(".gameboard > button");
+  const startButton = document.querySelector(".start-button > button");
+  const player1text = document.querySelector("#player1");
+  const player2text = document.querySelector("#player2");
+  const player1select = document.querySelector("#player1select");
+  const player2select = document.querySelector("#player2select");
 
   //Bind Events
-  gameBoard.forEach(_gameBoardClick);
+  startButton.addEventListener("click", _initGame);
+
+  //Bind Event Functions
+  function _removegameboardClick(item) {
+    item.removeEventListener("click", _playerClick);
+  }
   function _gameBoardClick(item) {
     item.addEventListener("click", _playerClick);
   }
 
+  //Bind event only if start button is clicked
+  function _initGame() {
+    if (startButton.innerText == "Start") {
+      gameBoard.forEach(_gameBoardClick);
+      _toggleButtonText();
+      _commitPlayerNames();
+    } else if (startButton.innerText == "Restart") {
+      _toggleButtonText();
+      gameBoard.forEach(_removegameboardClick);
+      _clearBoard();
+    }
+  }
+
   //Private Functions
+  function _clearBoard() {
+    counter = 0;
+    xsquares = [];
+    osquares = [];
+    for (let i = 0; i < gameBoard.length; i++) {
+      gameBoard[i].innerText = "";
+    }
+    player1text.value = "";
+    player2text.value = "";
+  }
+
   function _playerClick(e) {
     if (e.target.innerText == "") {
-      if (i % 2 == 0) {
+      if (counter % 2 == 0) {
         e.target.innerText = "X";
         xsquares.push(e.target.id);
         xsquares.sort(function (a, b) {
           return a - b;
         });
-        _checkWinner();
-        i++;
+        counter++;
+        _checkWinner(player1);
       } else {
         e.target.innerText = "O";
         osquares.push(e.target.id);
@@ -74,22 +108,56 @@ let gameBoard = (function () {
         osquares.sort(function (a, b) {
           return a - b;
         });
-
+        counter++;
         _checkWinner();
-        i++;
       }
     } else return;
   }
 
-  function _checkWinner() {
+  function _checkWinner(player1) {
+    console.log(player1.gamePiece);
+    console.log(player1.name);
     winningCombination.forEach((element) => {
       if (element.toString() === xsquares.toString()) {
-        alert("X wins");
-        _clearBoard();
+        if (player1.gamePiece == "X") {
+          alert(`${player1.name} wins`);
+        } else {
+          alert(`${player2.name} wins`);
+        }
+        _initGame();
       } else if (element.toString() === osquares.toString()) {
-        alert("O wins");
-        _clearBoard();
+        if (player1.gamePiece == "O") {
+          alert(`${player1.name} wins`);
+        } else {
+          alert(`${player2.name} wins`);
+        }
+        _initGame();
       }
     });
+  }
+
+  function _toggleButtonText() {
+    if (startButton.innerText == "Start") {
+      startButton.innerText = "Restart";
+    } else {
+      startButton.innerText = "Start";
+    }
+  }
+
+  function _commitPlayerNames() {
+    const playerFactory = (name, gamePiece) => {
+      return { name, gamePiece };
+    };
+
+    const player1 = playerFactory(
+      player1text.value == "" ? "Player 1" : player1text.value,
+      player1select.checked ? "O" : "X"
+    );
+    const player2 = playerFactory(
+      player2text.value == "" ? "Player 2" : player2text.value,
+      player2select.checked ? "O" : "X"
+    );
+
+    return { player1: gamePiece };
   }
 })();
